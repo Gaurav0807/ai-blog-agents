94250300b3a26f75c09fa815740ba48faa4a23b8
Gaurav Rawat <54076460+Gaurav0807@users.noreply.github.com>
2026-02-26 07:13:04 +0000

    ai-blog: fixing script

--- agent.py
+++ agent.py
@@ -1,5 +1,10 @@
+import sys
 from blog_graph import build_graph, BlogState
+import os
 
 if __name__ == "__main__":
+    target_path = sys.argv[1] if len(sys.argv) > 1 else "."
+    os.chdir(target_path)
+
     app = build_graph()
     app.invoke(BlogState())
